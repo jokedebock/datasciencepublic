89e6fb8ce5175207cf0f639a968aa7b0f69f64f4
JokeDeBock <joke@de-bock.com>
2019-05-28 16:19:16 +0200

    Code for exercise week 3 - exercise 1

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
 import pandas as pd
+import numpy as np
+
+d = pd.read_html("https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M")
+
+df = d[0]
+df.columns = ['PostalCode', 'Borough', 'Neighborhood']
+
+df = df.replace('Not assigned', np.nan)
+
+df = df.dropna(subset=['Borough'])
+
+df = df.groupby('Borough', as_index=False).agg(lambda x: ', '.join(set(x.dropna())))
+
+def fx(x):
+    if (x['Neighborhood']):
+        return x['Neighborhood']
+    else:
+        return x['Borough']
+
+df['Neighborhood'] = df.apply(lambda x : fx(x),axis=1)
+
+print(df.shape)
+
+
