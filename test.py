5caf1d3a1196384e5ba4e0cb602ccbe423f5e2e8
JokeDeBock <joke@de-bock.com>
2019-06-04 21:32:16 +0200

    Code for exercise week 3 - exercise 1

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from pypostalcode import PostalCodeDatabase
 
 d = pd.read_html("https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M")
 
@@ -10,7 +11,7 @@ df = df.replace('Not assigned', np.nan)
 
 df = df.dropna(subset=['Borough'])
 
-df = df.groupby('Borough', as_index=False).agg(lambda x: ', '.join(set(x.dropna())))
+df = df.groupby('PostalCode', as_index=False).agg(lambda x: ', '.join(set(x.dropna())))
 
 def fx(x):
     if (x['Neighborhood']):
@@ -20,6 +21,34 @@ def fx(x):
 
 df['Neighborhood'] = df.apply(lambda x : fx(x),axis=1)
 
-print(df.shape)
+#print(df)
 
+
+def searchlatitude(x):
+    #print(x)
+    pcdb = PostalCodeDatabase()
+    try:
+        location = pcdb[x]
+        #print(x, ", ", location.latitude)
+        return location.latitude
+    except:
+        return "Not found"
+
+def searchlongitude(x):
+    #print(x)
+    pcdb = PostalCodeDatabase()
+    try:
+        location = pcdb[x]
+        return location.longitude
+    except:
+        return "Not found"
+
+df['Latitude'] = df.apply(lambda row: searchlatitude(row.PostalCode), axis = 1)
+df['Longitude'] = df.apply(lambda row: searchlongitude(row.PostalCode), axis = 1)
+
+df = df.replace('Not found', np.nan)
+
+df = df.dropna(subset=['Latitude'])
+
+print(df)
 
